2bb96a4b33fe324a695ce3bd1356a357c27e74fd
Valeria Edwards <vedwardsn@ucenfotec.ac.cr>
2024-07-10 21:31:41 -0600

    Movies Genre added

---
+++ src/main/java/com/project/demo/logic/entity/genre/Genre.java
@@ -0,0 +1,32 @@
+package com.project.demo.logic.entity.genre;
+
+
+import jakarta.persistence.*;
+
+@Table(name = "movie_genre")
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
